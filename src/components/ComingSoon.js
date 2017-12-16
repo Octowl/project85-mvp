c4c45f93fb6c32c3e7b69df0231de064e32eda0c
Octowl <alsaff1987@gmail.com>
2017-12-17 00:49:42 +0300

    Add coming soon form

---
+++ src/components/ComingSoon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ComingSoon = () => (
+  <div className="main-content">
+    <h2>We're working hard to bring you new content</h2>
+    <iframe id="JotFormIFrame-73496416856470" onLoad={window.parent.scrollTo(0,0)} allowtransparency="true" src="https://form.jotform.me/73496416856470" frameBorder={0} style={{width:"100%", height:"539px", border:"none"}} scrolling="no">
+    </iframe>
+  </div>
+);
+
+export default ComingSoon;
