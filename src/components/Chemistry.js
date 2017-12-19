34101892b9620226bfb3e16062ce79b4b171f07e
Octowl <alsaff1987@gmail.com>
2017-12-19 17:43:41 +0300

    Add prelim youtube video

--- src/components/Chemistry.js
+++ src/components/Chemistry.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import YouTube from 'react-youtube';
 
 const Chemistry = () => (
   <div className="main-content">
     <h2>IGCSE Chemistry</h2>
+    <YouTube videoId="6LKm3S54Y3g"/>
   </div>
 );
 
